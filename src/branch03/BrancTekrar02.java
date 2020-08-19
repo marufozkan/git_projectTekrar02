a6944a0d59bb6214302526a6c546fc0dffbd0aed
maruf <mrfozkan47@gmail.com>
2020-08-19 12:45:46 +0300

    ikinci commit

--- src/branch03/BrancTekrar02.java
+++ src/branch03/BrancTekrar02.java
@@ -5,7 +5,7 @@ public class BrancTekrar02 {
 	public static void main(String[] args) {
 		System.out.println("saat 12:14 itibari ile branch `leri tekrar ediyorum");
 		
-		
+	
 System.out.println("saat 12:26 feature branc icin ugrasiyorum");
 
 
