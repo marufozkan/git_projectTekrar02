644489a12892ba1df13cd629de670d00ed57ec04
maruf <mrfozkan47@gmail.com>
2020-08-19 12:28:17 +0300

    feature branch

--- src/branch03/BrancTekrar02.java
+++ src/branch03/BrancTekrar02.java
@@ -3,8 +3,13 @@ package branch03;
 public class BrancTekrar02 {
 
 	public static void main(String[] args) {
-		
 		System.out.println("saat 12:14 itibari ile branch `leri tekrar ediyorum");
+		
+		
+System.out.println("saat 12:26 feature branc icin ugrasiyorum");
+
+
+
 
 	}
 
