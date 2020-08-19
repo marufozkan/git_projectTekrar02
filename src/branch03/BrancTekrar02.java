63f10c1929397e02ba926bba477827c2f448b954
maruf <mrfozkan47@gmail.com>
2020-08-19 12:20:57 +0300

    first commit

---
+++ src/branch03/BrancTekrar02.java
@@ -0,0 +1,11 @@
+package branch03;
+
+public class BrancTekrar02 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("saat 12:14 itibari ile branch `leri tekrar ediyorum");
+
+	}
+
+}
